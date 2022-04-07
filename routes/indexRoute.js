17189a9942dc9c96fa61a793120204bf6ee7e572
= <=>
2022-04-07 19:41:12 +0530

    Added Health Route

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,9 +3,9 @@ const controller = require('../api/index/Controller');
 
 const router = express.Router();
 
+router.get('/',(req,res)=> res.status(200).send('I am Working fine :}'));
 router.post('/user/login', (req, res)=>controller.login(req,res));
 router.get('/loginInfo/:a_id/:u_id', (req, res) => controller.loginInfo(req, res));
 router.get('/markAnswer/:a_id/:u_id', (req, res) => controller.markAnswer(req, res));
 router.get('/submit/:a_id/:u_id', (req, res) => controller.submit(req, res));
-
 module.exports = router;
