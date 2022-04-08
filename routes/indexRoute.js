945198f437a155ff1b31d7a76cb0f7eeddda946e
= <=>
2022-04-08 07:18:21 +0530

    added test post request

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,6 +4,7 @@ const controller = require('../api/index/Controller');
 const router = express.Router();
 
 router.get('/',(req,res)=> res.status(200).send('I am Working fine :}'));
+router.post('/',(req,res)=> res.status(201).send(`Recieved : ${JSON.stringify(req.body)}`))
 router.post('/user/login', (req, res)=>controller.login(req,res));
 router.get('/loginInfo/:a_id/:u_id', (req, res) => controller.loginInfo(req, res));
 router.get('/markAnswer/:a_id/:u_id', (req, res) => controller.markAnswer(req, res));
